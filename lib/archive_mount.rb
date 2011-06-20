091d075d30e97f8cb04b5e1c39bccce26ccd9f56
Ryan Taylor <ryanmt@byu.net>
2011-06-20 10:17:45 -0600

    changed to the OS gem for OS determination

--- lib/archive_mount.rb
+++ lib/archive_mount.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require 'os'
 # System dependent locations
 if OS.windows?
@@ -17,34 +16,37 @@ Db_mount = MountedServer::MountMapper.new(Database)
 	# I'm thinking that we can have a smart mount that knows your windows/linux location and thereby knows the actual location of every file you might need within the file structure.
 =begin
 	root = ..group/user/YYYYMM/experiment_name/
-	./init/ Files pertinent to the initialization of the data such as the TUNE and METHOD and UPLC files, and the UPLC pressure trace graph.
-	./metrics/ All the metrics stuff (Contains the comparison sets)
+	./init/ Files pertinent to the initialization of the data such as the TUNE and METHOD and UPLC files.
+	./metrics/ All the metrics stuff
 	./ident/ identification analysis
 	./quant/ quantification analysis
 	./results/ Results of the analysis
+	./graphs/ graphs of the Metrics and UPLC results (SO the HTML will rest here!)
 	./samplename(s).RAW
 	./mzML/ The mzXML and mzML files
 	./config.yml
 	./archive/ 	ANY other log files, or previous config files that might be stored
-
 =end
 
-
 class ArchiveMount
-  attr_accessor :base_path, :location 
 	def initialize(msrun)
 		@msrun = msrun
-    define_location
 	end
-@@build_directories = ['init', 'metrics', 'ident', 'quant', 'results', 'mzML', 'archive']
+@@build_directories = ['init', 'metrics', 'ident', 'quant', 'results', 'graphs', 'mzML', 'archive']
 #@location = [group, user, mtime, experiment_name]
 	def build_archive # @location == LOCATION(group, user, mtime, experiment_id)
-#		Dir.chdir(@location)
+		# cp the config file from the higher level down
 		@@build_directories.each do |dir|
 			mkdir dir
-# mdir File.join(@location, dir)
 		end
-    @location 
+# Find a config file and move it down to this directory... right?
+
+	end
+
+	def sys_check? # Returns an indication of the system you are on
+		out = "Windows" if ENV["OS"][/Windows/] == 'Windows'
+		out = "Linux" if ENV["HOME"][/\/home\//] == '/home/'
+		out
 	end
 
 	def to_mount
@@ -70,11 +72,10 @@ CygBin = "C:\\cygwin\\bin"
 CygHome = "C:\\cygwin\\home\\LTQ2"
 UserHost = 'ryanmt@jp1'
 ProgramLocale = '/home/ryanmt/Dropbox/coding/ms/archiver/lib/archiver.rb'
-Tmp_filename
 def send_msruninfo_to_linux_via_ssh(object)
-	File.open(Tmp_filename, 'w') {|out| YAML.dump(object, out)}
-	file_move = %Q[#{CygBin}\\scp #{Tmp_filename} #{UserHost}:/tmp/]
-	kick = %Q[#{CygBin}\\ssh #{UserHost} -C '#{ProgramLocale} --linux /tmp/#{Tmp_filename} ']
+	File.open('tmp.yml', 'w') {|out| YAML.dump(object, out)}
+	file_move = %Q[#{CygBin}\\scp tmp.yml #{UserHost}:/tmp/]
+	kick = %Q[#{CygBin}\\ssh #{UserHost} -C '#{ProgramLocale} --linux /tmp/tmp.yml ']
 	%x[#{kick}]
 	kick
 end
