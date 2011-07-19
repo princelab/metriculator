6d64bf3059a2b3c3209eef73da051361baa6f612
Jamison Dance <jergason@gmail.com>
2011-07-19 17:41:53 -0600

    Make the lib/ directory autoreload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,9 @@ MetricsSite::Application.configure do
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
+  config.autoload_paths += [File.join(Rails.root, "app")]
+  config.autoload_paths += [File.join(Rails.root, 'lib')]
+
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
