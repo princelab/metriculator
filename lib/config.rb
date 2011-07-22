4de4a5d444130c58eb78c5da3d827cee310e2246
Ryan Taylor <ryanmt@byu.net>
2011-07-22 17:23:06 -0600

    started thinking/working on the way to handle the parsing of the config settings

---
+++ lib/config.rb
@@ -0,0 +1,4 @@
+# I'm thinking that this will have fxns that load all the various settings from the yamls.  That shouldn't be too hard, but I'm not quite sure how to organize it.  
+# load_appconfig
+# load_runconfig
+# load_qcconfig 
