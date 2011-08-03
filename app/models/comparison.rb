ac4f35c433d2beab0f0931f35a5c7e2c96fa679b
Jamison Dance <jergason@gmail.com>
2011-08-03 13:15:08 -0600

    update graphing function to work with changed back-end

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -15,9 +15,9 @@ class Comparison
 
   #Produce a graph of the metrics in this comparison, or return it if it already exists.
   def graph
-    met = ::Ms::NIST::Metric.new("")
-    first = met.slice_matches self.msrun_firsts
-    second = met.slice_matches self.msrun_seconds
-    files = met.graph_matches first, second
+    grapher = ::Ms::ComparisonGrapher.new
+    first = grapher.slice_matches self.msrun_firsts
+    second = grapher.slice_matches self.msrun_seconds
+    files = grapher.graph_matches first, second
   end
 end
