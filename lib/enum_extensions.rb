9f5399a00610d7737c98e8e66b8dad1ea0d66bae
Ryan Taylor <ryanmt@byu.net>
2011-08-05 18:42:12 -0600

    added stats fxns to the Enum class to allow for computation of things within ruby

---
+++ lib/enum_extensions.rb
@@ -0,0 +1,28 @@
+# Adapted from https://www.bcg.wisc.edu/webteam/support/ruby/standard_deviation
+#  Add methods to Enumerable, which makes them available to Array
+module Enumerable
+ 
+  #  sum of an array of numbers
+  def sum
+    self.inject(:+)
+  end
+ 
+  #  average of an array of numbers
+  def average
+    self.sum/self.length.to_f
+  end
+  alias :mean :average
+
+  #  variance of an array of numbers
+  def sample_variance
+    avg=self.average
+    sum=self.inject(0){|acc,i|acc +(i-avg)**2}
+    (1/self.length.to_f*sum)
+  end
+ 
+  #  standard deviation of an array of numbers
+  def standard_deviation
+    Math.sqrt(self.sample_variance)
+  end
+ 
+end  #  module Enumerable
