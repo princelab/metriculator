987aeacc7f342e323a3da39bcb40d98e5cb11492
Jamison Dance <jergason@gmail.com>
2011-08-08 16:23:54 -0600

    load alerts before every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ require 'dm-rails/middleware/identity_map'
 class ApplicationController < ActionController::Base
   use Rails::DataMapper::Middleware::IdentityMap
   protect_from_forgery
+
+  before :load_alerts
+
+  def load_alerts
+    @alerts = Alerts.all
+  end
 end
