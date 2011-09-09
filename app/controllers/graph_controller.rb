92ad3addd8bc74453131509ad8fe4b6e5f3ba4f2
Jamison Dance <jergason@gmail.com>
2011-09-09 13:58:00 -0600

    delete unneeded controllers

--- app/controllers/graph_controller.rb
+++
@@ -1,7 +0,0 @@
-class GraphController < ApplicationController
-  def show
-    @title = 'hello'
-    @entry_dir = Dir.pwd
-    @category = 'chromatography'.to_sym
-  end
-end
--- app/controllers/msrun_controller.rb
+++
@@ -1,7 +0,0 @@
-class MsrunController < ApplicationController
-  def show
-  end
-  def index
-    @msruns = Msrun.all 
-  end
-end
