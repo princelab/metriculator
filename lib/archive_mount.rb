ec5dcd8467e5559056f72af32b5cf1901deab259
Ryan Taylor <ryanmt@byu.net>
2011-11-14 20:56:23 -0700

    fixing xcalibur, still...

--- lib/archive_mount.rb
+++ lib/archive_mount.rb
@@ -49,7 +49,7 @@ module Ms
       # @param None, uses #msrun initialized
       # @return location array
       def define_location
-	tmp = [@msrun.group, @msrun.user, File.mtime(@msrun.rawfile), @msrun.rawid].zip( ["No Group", "No User", Time.now, "Never see this"] ).map {|a| a.first.nil? ? a.last : a.first }  
+	tmp = [@msrun.group, @msrun.user, File.mtime(@msrun.rawfile), @msrun.rawid].zip( ["No Group", "No User", "#{Time.now}", "Never see this"] ).map {|a| a.first.nil? ? a.last : a.first }  
         @location = File.join(ArchiveRoot, tmp)
 	binding.pry
         @msrun.archive_location = @location
