98014997856a138bc4ae5b4dd44a8739eca243b0
Jamison Dance <jergason@gmail.com>
2011-12-02 12:02:06 -0700

    remove duplicate jquery files and add in humanejs

--- public/javascripts/msruns.js
+++ public/javascripts/msruns.js
@@ -1,4 +1,5 @@
 $(function() {
+  "use strict";
 
   $("#search_rawtime_start").live("click", function() {
     $(this).datepicker({dateFormat: "yy-mm-dd",});
@@ -11,19 +12,23 @@ $(function() {
   // in ALL search forms.
   $("button#filter").live("click", function () {
     //Build the query string from the filter forms
-    var parms = [];
+    var parms = []
+      , query_string
+      ;
     $("form div.filter-form :input").each(function(i, obj) {
+      var query_string_fragment
+        ;
       if (obj.type === "submit" || obj.value === "") {
         return;
       }
-      var query_string = obj.name + "=" + obj.value;
-      console.log(query_string);
-      if (parms.indexOf(query_string) === -1) {
-        parms.push(query_string);
+      query_string_fragment = obj.name + "=" + obj.value;
+      console.log(query_string_fragment);
+      if (parms.indexOf(query_string_fragment) === -1) {
+        parms.push(query_string_fragment);
       }
     });
 
-    var query_string = parms.join("&");
+    query_string = parms.join("&");
     console.log(query_string);
 
     $.get("msruns?" + query_string, function(data, status, jqXHR) {
@@ -41,12 +46,14 @@ $(function() {
   });
 
   $("#msruns td.comparison1 button").live("click", function() {
-    var res = RailsMetrics.addToComparisonSet("1", this);
+    var res = RailsMetrics.addToComparisonSet("1", this)
+      , stamp
+      ;
     if (res) {
-      var stamp = new Date().getTime();
+      stamp = new Date().getTime();
       $("span").attr({
-        id: stamp,
-        "class": ""
+        "id": stamp,
+        "class": stamp
       }).text("Comparison added to Comparison 1.").appendTo("#messages");
       setTimeout(function() {
         $("#"+stamp).hide("fold");
@@ -56,13 +63,15 @@ $(function() {
 
   //UGHH Repeated code
   $("#msruns td.comparison2 button").live("click", function() {
-    var res = RailsMetrics.addToComparisonSet("2", this);
+    var res = RailsMetrics.addToComparisonSet("2", this)
+      , stamp
+      ;
 
     if (res) {
-      var stamp = new Date().getTime();
+      stamp = new Date().getTime();
       $("span").attr({
-        id: stamp,
-        class: stamp 
+        "id": stamp,
+        "class": stamp
       }).text("Comparison added to Comparison 2.").appendTo("#messages");
       setTimeout(function() {
         $("."+stamp).hide("fold");
@@ -70,46 +79,3 @@ $(function() {
     }
   });
 });
-
-RailsMetrics.addToComparisonSet = function(whichComparisonSet, clickedObject) {
-  var comparisonID = clickedObject.id.split(/-/)[2];
-  var comparisonName = $("#msruns tr#msrunid-"+comparisonID).children().eq(2).text();
-  //TODO: Check if it is in the select list already. If not, add it in.
-  var option = "<option value='"+ comparisonID + "' selected>" + comparisonName + "</option>";
-  var isAlreadyIncluded = false;
-  $("#comparison" + whichComparisonSet).children().each(function(i, comp) {
-    if (comp.value == comparisonID) {
-      isAlreadyIncluded = true;
-    }
-  });
-  if (!isAlreadyIncluded) {
-    $("#comparison"+whichComparisonSet).append(option);
-    // TODO: Add some kind of flash notice that it was added.
-    return true;
-  }
-  return false;
-};
-
-RailsMetrics.allButtonClicked = function(whichComparisonSet) {
-  var count = 0;
-  $("#msruns tr").each(function(i, row) {
-    if (row.id !== "") {
-      //We are at a row that has data
-      var added = RailsMetrics.addToComparisonSet(whichComparisonSet, $(row).children().eq(parseInt(whichComparisonSet , 10) - 1).children()[0]);
-      if (added) {
-        count++;
-      }
-    }
-  });
-  if (count > 0) {
-    //Make a message appear.
-    var stamp = new Date().getTime();
-    $("span").attr({
-      id: stamp,
-      "class": ""
-    }).text("" + count + " comaprisons added to Comparison Set " + whichComparisonSet + ".").appendTo("#messages");
-    setTimeout(function() {
-      $("#"+stamp).hide("fold");
-    }, 5000);
-  }
-};
