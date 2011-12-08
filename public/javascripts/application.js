133fcbd8a766e30ebea3dda0bd0e9e7d0fc72b31
Russ Taylor <russ@russ-taylor.com>
2011-12-08 16:45:46 -0700

    Replaced 'live' with 'click' in jQuery - 'live' is deprecated.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
-  $("#msruns th a, #msruns li a").live("click", function() {
+  $("#msruns th a, #msruns li a").click(function(event) {
     $.getScript(this.href);
     return false;
   });
 
   // Fade the alerts out when the delete link is clicked.
-  $("#alerts span a").live("click", function() {
+  $("#alerts span a").click(function(event) {
     $(this).parent().fadeOut()
   });
 
