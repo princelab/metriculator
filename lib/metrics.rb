c854a1de3f567c64a1dfd3a5c12638093be3d4ba
Ryan Taylor <ryanmt@byu.net>
2012-01-23 13:43:34 -0700

    update

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -69,6 +69,8 @@ module Ms
           path = ArchiveMount.cp_to(@rawfile, tmp_id.to_s)
         else
           path = File.dirname(@rawfile)
+          puts path
+          puts @rawfile
         end
         output_metrics_file = File.join(path, File.basename(@rawfile, '.raw'))
         putsv "PATH: #{path}"
