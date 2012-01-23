81d9fbe1f67d8f285264f76218d0d033b42d1106
Ryan Taylor <ryanmt@byu.net>
2012-01-23 13:53:57 -0700

    update

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -69,7 +69,7 @@ module Ms
         output_metrics_file = File.join(path, File.basename(@rawfile, '.raw'))
         putsv "PATH: #{path}"
         putsv "output_metrics_file: #{output_metrics_file}"
-        putsv "#{ArchiveMount.config.metric_instrument_type}"
+        #putsv "#{ArchiveMount.config.metric_instrument_type}"
         instrument_type = "ORBI"
         %Q{#{::NistProgram} --in_dir "#{path}" --out_file "#{output_metrics_file}" --library #{ArchiveMount.config.metric_taxonomy}  --instrument_type #{instrument_type} }
         ## PARSE THE FILE
